5c3a260f89856957c0d31e383aca33a5b63e0afc
Jason Dew <jason.dew@gmail.com>
2010-05-27 14:18:51 -0400

    trying something new

--- lib/generators/shoulda_with_factory_girl/scaffold/scaffold_generator.rb
+++ lib/generators/shoulda_with_factory_girl/scaffold/scaffold_generator.rb
@@ -1,22 +1,16 @@
 require 'generators/shoulda_with_factory_girl'
-require 'rails/generators/test_unit/scaffold/scaffold_generator'
 
 module ShouldaWithFactoryGirl
   module Generators
-    class ScaffoldGenerator < TestUnit::Generators::ScaffoldGenerator
+    class ScaffoldGenerator < Base
+      argument :actions, :type => :array, :default => [], :banner => "action action"
+      class_option :dir, :type => :string, :default => "test/functional", :desc => "The directory where the controller tests should go"
 
-      include Rails::Generators::ResourceHelpers
+      check_class_collision :suffix => 'ControllerTest'
 
-      class_option :functional_test_dir, :type => :string, :default => "test/functional", :desc => "The directory where the controller tests should go"
-      class_option :unit_test_dir, :type => :string, :default => "test/unit", :desc => "The directory where the controller tests should go"
-      check_class_collision :suffix => "ControllerTest"
-
-      def create_test_files
-        template 'model.rb', File.join(options[:unit_test_dir], "#{singular_name}_test.rb")
-        template 'controller.rb', File.join(options[:functional_test_dir], "#{file_name}_controller_test.rb")
+      def create_controller_file
+        template 'controller.rb', File.join(options[:dir], "#{file_name}_controller_test.rb")
       end
-
-      hook_for :fixture_replacement
     end
   end
 end
--- lib/generators/shoulda_with_factory_girl/scaffold/templates/model.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class <%= class_name %>Test < ActiveSupport::TestCase
-
-  should "be valid from the factory" do
-    assert_valid Factory.build(:<%= singular_name %>)
-  end
-
-end
