5fd521f7d2fd0d78082c9e0a1315fb8ac77affbb
Jason Dew <jason.dew@gmail.com>
2010-05-27 14:15:42 -0400

    separating out options for the scaffold generator

--- lib/generators/shoulda_with_factory_girl/scaffold/scaffold_generator.rb
+++ lib/generators/shoulda_with_factory_girl/scaffold/scaffold_generator.rb
@@ -7,12 +7,13 @@ module ShouldaWithFactoryGirl
 
       include Rails::Generators::ResourceHelpers
 
-      class_option :dir, :type => :string, :default => "test/functional", :desc => "The directory where the controller tests should go"
+      class_option :functional_test_dir, :type => :string, :default => "test/functional", :desc => "The directory where the controller tests should go"
+      class_option :unit_test_dir, :type => :string, :default => "test/unit", :desc => "The directory where the controller tests should go"
       check_class_collision :suffix => "ControllerTest"
 
       def create_test_files
-        template 'model.rb', File.join(options[:dir], "#{singular_name}_test.rb")
-        template 'controller.rb', File.join(options[:dir], "#{file_name}_controller_test.rb")
+        template 'model.rb', File.join(options[:unit_test_dir], "#{singular_name}_test.rb")
+        template 'controller.rb', File.join(options[:functional_test_dir], "#{file_name}_controller_test.rb")
       end
 
       hook_for :fixture_replacement
